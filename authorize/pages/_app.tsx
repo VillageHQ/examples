29ba36be57463451c449bba0b0d8f6de5e58c556
Rafael Muttoni <rafael@muttoni.com.br>
2025-04-14 13:04:45 +0700

    env example + VILLAGE_SCRIPT_URL

--- authorize/pages/_app.tsx
+++ authorize/pages/_app.tsx
@@ -3,13 +3,14 @@ import type { AppProps } from "next/app";
 import Script from "next/script";
 
 const VILLAGE_PUBLIC_KEY = process.env.NEXT_PUBLIC_VILLAGE_PUBLIC_KEY;
+const VILLAGE_SCRIPT_URL = process.env.NEXT_PUBLIC_VILLAGE_SCRIPT_URL;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script id="village-script" strategy="beforeInteractive">
         {`
-          (function(){var w=window;var d=document;var v=w.Village||{};d.head.appendChild(Object.assign(d.createElement("style"),{textContent:'[village-paths-availability="found"],[village-paths-availability="not-found"]{display:none}'}));v.q=v.q||[];v._call=function(method,args){v.q.push([method,args])};v.init=function(){v._call("init",arguments)};v.identify=function(){v._call("authorize",arguments)};w.Village=v;var l=function(){var s=d.createElement("script");s.type="text/javascript";s.async=true;s.src="https://js.village.do";var x=d.getElementsByTagName("script")[0];x.parentNode.insertBefore(s,x)};if(w.Village.loaded)return;if(w.attachEvent){w.attachEvent("onload",l)}else{w.addEventListener("load",l,false)}w.Village.loaded=true})();
+          (function(){var w=window;var d=document;var v=w.Village||{};d.head.appendChild(Object.assign(d.createElement("style"),{textContent:'[village-paths-availability="found"],[village-paths-availability="not-found"]{display:none}'}));v.q=v.q||[];v._call=function(method,args){v.q.push([method,args])};v.init=function(){v._call("init",arguments)};v.identify=function(){v._call("authorize",arguments)};w.Village=v;var l=function(){var s=d.createElement("script");s.type="text/javascript";s.async=true;s.src="${VILLAGE_SCRIPT_URL}";var x=d.getElementsByTagName("script")[0];x.parentNode.insertBefore(s,x)};if(w.Village.loaded)return;if(w.attachEvent){w.attachEvent("onload",l)}else{w.addEventListener("load",l,false)}w.Village.loaded=true})();
           Village.init('${VILLAGE_PUBLIC_KEY}');
         `}
       </Script>
